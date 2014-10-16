899a22df14f3a8fd866d3108170b2956a0407d5c
Alan Lee <coderiff@gmail.com>
2014-10-16 23:24:28 +0900

    added demo script

--- sapy/sapy.py
+++ sapy/sapy.py
@@ -3,6 +3,9 @@
 import webapp2
 import urllib2
 import re
+import logging
+
+logging.getLogger().setLevel(logging.DEBUG)
 
 class ProxyHandler(webapp2.RequestHandler):
     def __init__(self, request, response):
@@ -10,16 +13,25 @@ class ProxyHandler(webapp2.RequestHandler):
         self.initialize(request, response)
         
         # TODO: Add host address used for remote query
-        self.base = 'http://api.openweathermap.org'
+        self.base = 'https://qrng.anu.edu.au'
 
     def remote_fetch(self, url, data = None):
         ''' fetch HTTP Response from remote host and create new response with CORS header '''
 
         try:
+          logging.debug(url)
           result = urllib2.urlopen(url, data)
-          self.response.headers["Content-Type"] = result.info().get('content-type')
+          response_text = result.read()
+
+          content_type = result.info().get('content-type')
+          # replace 'text/javascript' (obsoleted by RFC 4329) with 'application/json'
+          if content_type == 'text/javascript':
+              content_type = 'application/json'
+
+          self.response.headers["Content-Type"] =  content_type
           self.response.headers.add_header("Access-Control-Allow-Origin", "*") # CORS header
-          self.response.write(result.read())
+          logging.debug(response_text)
+          self.response.write(response_text)
         except urllib2.URLError, e:
           self.response.write(str(e.reason))
 
